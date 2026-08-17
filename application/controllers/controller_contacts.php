b387d7f8b60354da0e2d8b2bfba05797c5af2b7d
Ramfour <bdddanil2002@gmail.com>
2026-08-17 15:14:30 +0700

    add new text

--- application/controllers/controller_contacts.php
+++ application/controllers/controller_contacts.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Summary of Controller_Contacts
+ * 
+ * @extends Controller {@see Application\Controllers}
+ */
 class Controller_Contacts extends Controller { 
 function action_index() { 
 $this->view->generate('contacts_view.php', 'template_view.php'); 
